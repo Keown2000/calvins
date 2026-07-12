2604e08ffeb892695d1fe119b03b493ed1016bd4
Keown2000 <64482219+Keown2000@users.noreply.github.com>
2026-07-12 16:37:11 +1000

    Update menu.js

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,1 +1,26 @@
+const menu = [
+  {
+    id: 1,
+    name: "Signature Steak",
+    description: "Premium cut steak with seasonal vegetables",
+    price: "$38",
+    image: "/images/steak.jpg",
+  },
+  {
+    id: 2,
+    name: "Truffle Pasta",
+    description: "Fresh handmade pasta with black truffle",
+    price: "$28",
+    image: "/images/pasta.jpg",
+  },
+  {
+    id: 3,
+    name: "Calvin's Burger",
+    description: "House-made brioche bun with gourmet beef",
+    price: "$24",
+    image: "/images/burger.jpg",
+  },
+];
+
+export default menu;
 
