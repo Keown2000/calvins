1df10bbe29129ecfdc6cee0f3636dd26cdb6a675
Keown2000 <64482219+Keown2000@users.noreply.github.com>
2026-07-12 16:40:45 +1000

    Update FeaturedMenu.jsx

--- src/components/FeaturedMenu.jsx
+++ src/components/FeaturedMenu.jsx
@@ -1,1 +1,28 @@
+import menu from "../data/menu";
+import MenuCard from "./MenuCard";
+
+function FeaturedMenu() {
+  return (
+    <section className="featured-menu" id="menu">
+
+      <h2>
+        Featured Dishes
+      </h2>
+
+      <div className="menu-grid">
+
+        {menu.map((item) => (
+          <MenuCard
+            key={item.id}
+            item={item}
+          />
+        ))}
+
+      </div>
+
+    </section>
+  );
+}
+
+export default FeaturedMenu;
 
