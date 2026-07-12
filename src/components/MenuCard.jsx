7b4131157d585613e7f5506a8cc59b7250eb11ee
Keown2000 <64482219+Keown2000@users.noreply.github.com>
2026-07-12 16:39:29 +1000

    Update MenuCard.jsx

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,1 +1,31 @@
+function MenuCard({ item }) {
+  return (
+    <div className="menu-card">
+
+      <img 
+        src={item.image}
+        alt={item.name}
+      />
+
+      <div className="menu-info">
+
+        <h3>
+          {item.name}
+        </h3>
+
+        <p>
+          {item.description}
+        </p>
+
+        <span>
+          {item.price}
+        </span>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default MenuCard;
 
